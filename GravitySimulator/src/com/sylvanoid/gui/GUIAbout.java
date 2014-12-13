6361e320e993965668fdf1db7f48e3dfe97591ca
sylvain <sylvain@debian-desktop.localhost>
2014-12-13 11:45:03 +0100

    ...

--- GravitySimulator/src/com/sylvanoid/gui/GUIAbout.java
+++ GravitySimulator/src/com/sylvanoid/gui/GUIAbout.java
@@ -27,7 +27,13 @@ public class GUIAbout extends JDialog {
 				(mother.getHeight() - h) / 2));
 		setSize(new Dimension(w, h));
 		setLayout(new BorderLayout());
-		String text = "<html><body>Gravity Simulator<br/>V0.9 Beta<br/>GNU GENERAL PUBLIC LICENSE<br/>Credit: Sylvain Billot<br/>https://github.com/SylvainBillot/GravitySimulator</body></html>";
+		String text = "<html><body>Gravity Simulator<br/>" +
+				"V0.9 Beta<br/>GNU GENERAL PUBLIC LICENSE<br/>" +
+				"Credit: Sylvain Billot<br/>" +
+				"https://github.com/SylvainBillot/GravitySimulator<br/>" +
+				"Credit: JOGL 2<br/>" +
+				"Credit: jcodec-0.1.5<br/>" +
+				"</body></html>";
 		JLabel content = new JLabel(text);
 		content.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 		add(content, BorderLayout.CENTER);
