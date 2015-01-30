3150c0112bd504c5cbd01119b642cb8a036e4e7a
sylvain <sylvain@debian-desktop.localhost>
2015-01-30 17:59:37 +0100

    ...

--- GravitySimulator/src/com/sylvanoid/joblib/BarnesHut.java
+++ GravitySimulator/src/com/sylvanoid/joblib/BarnesHut.java
@@ -29,166 +29,145 @@ public class BarnesHut extends RecursiveTask<Integer> {
 		if (univers.getMass() > parameters.getNegligeableMass()
 				&& !univers.sameCoordonate()) {
 			parameters.setNumOfCompute(parameters.getNumOfCompute() + 1);
-			if (parameters.isManageImpact() && parameters.isFusion()
-					&& univers.getVolumicMass() > parameters.getDensity()) {
-				Matter mFirst = new Matter();
-				boolean firstTime = true;
-				for (Matter m : univers.getListMatter()) {
-					if (firstTime && !m.isDark()) {
-						mFirst = m;
-						firstTime = false;
+			double cx = univers.getMin().x
+					+ (univers.getMax().x - univers.getMin().x) / 2;
+			double cy = univers.getMin().y
+					+ (univers.getMax().y - univers.getMin().y) / 2;
+			double cz = univers.getMin().z
+					+ (univers.getMax().z - univers.getMin().z) / 2;
+
+			Univers suba = new Univers(univers, new Vector3d(cx, cy, cz),
+					new Vector3d(univers.getMax().x, univers.getMax().y,
+							univers.getMax().z));
+			Univers subb = new Univers(univers, new Vector3d(cx, cy,
+					univers.getMin().z), new Vector3d(univers.getMax().x,
+					univers.getMax().y, cz));
+			Univers subc = new Univers(univers, new Vector3d(cx,
+					univers.getMin().y, cz), new Vector3d(univers.getMax().x,
+					cy, univers.getMax().z));
+			Univers subd = new Univers(univers, new Vector3d(cx,
+					univers.getMin().y, univers.getMin().z), new Vector3d(
+					univers.getMax().x, cy, cz));
+			Univers sube = new Univers(univers, new Vector3d(
+					univers.getMin().x, cy, cz), new Vector3d(cx,
+					univers.getMax().y, univers.getMax().z));
+			Univers subf = new Univers(univers, new Vector3d(
+					univers.getMin().x, cy, univers.getMin().z), new Vector3d(
+					cx, univers.getMax().y, cz));
+			Univers subg = new Univers(univers, new Vector3d(
+					univers.getMin().x, univers.getMin().y, cz), new Vector3d(
+					cx, cy, univers.getMax().z));
+			Univers subh = new Univers(univers,
+					new Vector3d(univers.getMin().x, univers.getMin().y,
+							univers.getMin().z), new Vector3d(cx, cy, cz));
+
+			for (Matter m : univers.getListMatter()) {
+				if (m.getPoint().x > cx) {
+					if (m.getPoint().y > cy) {
+						if (m.getPoint().z > cz) {
+							suba.getListMatter().add(m);
+						} else {
+							subb.getListMatter().add(m);
+						}
 					} else {
-						if (!m.isDark()) {
-							mFirst.getFusionWith().add(m);
+						if (m.getPoint().z > cz) {
+							subc.getListMatter().add(m);
+						} else {
+							subd.getListMatter().add(m);
+						}
+					}
+				} else {
+					if (m.getPoint().y > cy) {
+						if (m.getPoint().z > cz) {
+							sube.getListMatter().add(m);
+						} else {
+							subf.getListMatter().add(m);
+						}
+					} else {
+						if (m.getPoint().z > cz) {
+							subg.getListMatter().add(m);
+						} else {
+							subh.getListMatter().add(m);
 						}
 					}
 				}
-			} else {
+			}
 
-				double cx = univers.getMin().x
-						+ (univers.getMax().x - univers.getMin().x) / 2;
-				double cy = univers.getMin().y
-						+ (univers.getMax().y - univers.getMin().y) / 2;
-				double cz = univers.getMin().z
-						+ (univers.getMax().z - univers.getMin().z) / 2;
+			List<Univers> subUnivers = new ArrayList<Univers>();
+			subUnivers.add(suba);
+			subUnivers.add(subb);
+			subUnivers.add(subc);
+			subUnivers.add(subd);
+			subUnivers.add(sube);
+			subUnivers.add(subf);
+			subUnivers.add(subg);
+			subUnivers.add(subh);
 
-				Univers suba = new Univers(univers, new Vector3d(cx, cy, cz),
-						new Vector3d(univers.getMax().x, univers.getMax().y,
-								univers.getMax().z));
-				Univers subb = new Univers(univers, new Vector3d(cx, cy,
-						univers.getMin().z), new Vector3d(univers.getMax().x,
-						univers.getMax().y, cz));
-				Univers subc = new Univers(univers, new Vector3d(cx,
-						univers.getMin().y, cz), new Vector3d(
-						univers.getMax().x, cy, univers.getMax().z));
-				Univers subd = new Univers(univers, new Vector3d(cx,
-						univers.getMin().y, univers.getMin().z), new Vector3d(
-						univers.getMax().x, cy, cz));
-				Univers sube = new Univers(univers, new Vector3d(
-						univers.getMin().x, cy, cz), new Vector3d(cx,
-						univers.getMax().y, univers.getMax().z));
-				Univers subf = new Univers(univers, new Vector3d(
-						univers.getMin().x, cy, univers.getMin().z),
-						new Vector3d(cx, univers.getMax().y, cz));
-				Univers subg = new Univers(univers, new Vector3d(
-						univers.getMin().x, univers.getMin().y, cz),
-						new Vector3d(cx, cy, univers.getMax().z));
-				Univers subh = new Univers(univers, new Vector3d(
-						univers.getMin().x, univers.getMin().y,
-						univers.getMin().z), new Vector3d(cx, cy, cz));
+			suba.computeMassLimitsCentroidSpeed(false);
+			subb.computeMassLimitsCentroidSpeed(false);
+			subc.computeMassLimitsCentroidSpeed(false);
+			subd.computeMassLimitsCentroidSpeed(false);
+			sube.computeMassLimitsCentroidSpeed(false);
+			subf.computeMassLimitsCentroidSpeed(false);
+			subg.computeMassLimitsCentroidSpeed(false);
+			subh.computeMassLimitsCentroidSpeed(false);
 
-				for (Matter m : univers.getListMatter()) {
-					if (m.getPoint().x > cx) {
-						if (m.getPoint().y > cy) {
-							if (m.getPoint().z > cz) {
-								suba.getListMatter().add(m);
-							} else {
-								subb.getListMatter().add(m);
-							}
-						} else {
-							if (m.getPoint().z > cz) {
-								subc.getListMatter().add(m);
-							} else {
-								subd.getListMatter().add(m);
-							}
-						}
-					} else {
-						if (m.getPoint().y > cy) {
-							if (m.getPoint().z > cz) {
-								sube.getListMatter().add(m);
-							} else {
-								subf.getListMatter().add(m);
-							}
-						} else {
-							if (m.getPoint().z > cz) {
-								subg.getListMatter().add(m);
-							} else {
-								subh.getListMatter().add(m);
-							}
-						}
-					}
-				}
+			BarnesHut bha = new BarnesHut(suba);
+			BarnesHut bhb = new BarnesHut(subb);
+			BarnesHut bhc = new BarnesHut(subc);
+			BarnesHut bhd = new BarnesHut(subd);
+			BarnesHut bhe = new BarnesHut(sube);
+			BarnesHut bhf = new BarnesHut(subf);
+			BarnesHut bhg = new BarnesHut(subg);
+			BarnesHut bhh = new BarnesHut(subh);
 
-				List<Univers> subUnivers = new ArrayList<Univers>();
-				subUnivers.add(suba);
-				subUnivers.add(subb);
-				subUnivers.add(subc);
-				subUnivers.add(subd);
-				subUnivers.add(sube);
-				subUnivers.add(subf);
-				subUnivers.add(subg);
-				subUnivers.add(subh);
+			// Parallelization
+			/*
+			 * bha.fork(); bhb.fork(); bhc.fork(); bhd.fork(); bhe.fork();
+			 * bhf.fork(); bhg.fork(); bhh.fork();
+			 * 
+			 * bha.join(); bhb.join(); bhc.join(); bhd.join(); bhe.join();
+			 * bhf.join(); bhg.join(); bhh.join();
+			 */
 
-				suba.computeMassLimitsCentroidSpeed(false);
-				subb.computeMassLimitsCentroidSpeed(false);
-				subc.computeMassLimitsCentroidSpeed(false);
-				subd.computeMassLimitsCentroidSpeed(false);
-				sube.computeMassLimitsCentroidSpeed(false);
-				subf.computeMassLimitsCentroidSpeed(false);
-				subg.computeMassLimitsCentroidSpeed(false);
-				subh.computeMassLimitsCentroidSpeed(false);
+			bha.compute();
+			bhb.compute();
+			bhc.compute();
+			bhd.compute();
+			bhe.compute();
+			bhf.compute();
+			bhg.compute();
+			bhh.compute();
 
-				BarnesHut bha = new BarnesHut(suba);
-				BarnesHut bhb = new BarnesHut(subb);
-				BarnesHut bhc = new BarnesHut(subc);
-				BarnesHut bhd = new BarnesHut(subd);
-				BarnesHut bhe = new BarnesHut(sube);
-				BarnesHut bhf = new BarnesHut(subf);
-				BarnesHut bhg = new BarnesHut(subg);
-				BarnesHut bhh = new BarnesHut(subh);
-
-				// Parallelization
-				/*
-				 * bha.fork(); bhb.fork(); bhc.fork(); bhd.fork(); bhe.fork();
-				 * bhf.fork(); bhg.fork(); bhh.fork();
-				 * 
-				 * bha.join(); bhb.join(); bhc.join(); bhd.join(); bhe.join();
-				 * bhf.join(); bhg.join(); bhh.join();
-				 */
-
-				bha.compute();
-				bhb.compute();
-				bhc.compute();
-				bhd.compute();
-				bhe.compute();
-				bhf.compute();
-				bhg.compute();
-				bhh.compute();
-
-				for (Univers u : subUnivers) {
-					for (Univers uvoisin : subUnivers) {
-						if (u != uvoisin
-								&& uvoisin.getListMatter().size() > 0
-								&& uvoisin.getMass() > parameters
-										.getNegligeableMass()) {
-							for (Matter m : u.getListMatter()) {
-								parameters.setNumOfAccelCompute(parameters
-										.getNumOfAccelCompute() + 1);
-								double distance = new Point3d(m.getPoint())
-										.distance(new Point3d(uvoisin
-												.getGPoint()));
-
-								double attraction = parameters.getTimeFactor()
-										* HelperVariable.G
-										* (((uvoisin.getMass()) / net.jafama.FastMath
-												.pow2(distance)));
-
+			for (Univers u : subUnivers) {
+				for (Univers uvoisin : subUnivers) {
+					if (u != uvoisin
+							&& uvoisin.getListMatter().size() > 0
+							&& uvoisin.getMass() > parameters
+									.getNegligeableMass()) {
+						for (Matter m : u.getListMatter()) {
+							parameters.setNumOfAccelCompute(parameters
+									.getNumOfAccelCompute() + 1);
+							double distance = new Point3d(m.getPoint())
+									.distance(new Point3d(uvoisin.getGPoint()));
+							double attraction = parameters.getTimeFactor()
+									* HelperVariable.G
+									* (((uvoisin.getMass()) / net.jafama.FastMath
+											.pow2(distance)));
+							if (!parameters.isManageImpact()
+									|| uvoisin.getListMatter().size() > 1
+									|| m.isDark() != uvoisin.getListMatter()
+											.get(0).isDark()
+									|| (distance > m.getRayon()
+											+ uvoisin.getListMatter().get(0)
+													.getRayon())) {
 								m.getSpeed()
 										.add(HelperVector.acceleration(
 												m.getPoint(),
 												uvoisin.getGPoint(), attraction));
-								/*
-								 * if (!parameters.isManageImpact() ||
-								 * uvoisin.getListMatter().size() > 1 ||
-								 * m.isDark() != uvoisin .getListMatter().get(0)
-								 * .isDark() || (distance > m.getRayon() +
-								 * uvoisin.getListMatter() .get(0).getRayon()))
-								 * { m.getSpeed().add(
-								 * HelperVector.acceleration( m.getPoint(),
-								 * uvoisin.getGPoint(), attraction)); } else {
-								 * m.getFusionWith().add(
-								 * uvoisin.getListMatter().get(0)); }
-								 */
+							} else {
+								m.getFusionWith().add(
+										uvoisin.getListMatter().get(0));
 							}
 						}
 					}
