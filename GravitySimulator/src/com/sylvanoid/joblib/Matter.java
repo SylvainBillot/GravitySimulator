13c1e5531842848a104bea4ba3f94495f3d2c274
sylvain <sylvain@sylvain-P17SM-A>
2015-08-05 20:24:37 +0200

    ...

--- GravitySimulator/src/com/sylvanoid/joblib/Matter.java
+++ GravitySimulator/src/com/sylvanoid/joblib/Matter.java
@@ -278,22 +278,20 @@ public class Matter implements Serializable {
 
 	public void impact() {
 		for (Matter m : fusionWith) {
-			if (m.getFusionWith().contains(this)) {
-				double Cr = parameters.getTypeOfImpact();
-				double v1x = (Cr * m.getMass() * (m.getSpeed().x - speed.x)
-						+ mass * speed.x + m.getMass() * m.getSpeed().x)
-						/ (mass + m.getMass());
-				double v1y = (Cr * m.getMass() * (m.getSpeed().y - speed.y)
-						+ mass * speed.y + m.getMass() * m.getSpeed().y)
-						/ (mass + m.getMass());
-				double v1z = (Cr * m.getMass() * (m.getSpeed().z - speed.z)
-						+ mass * speed.z + m.getMass() * m.getSpeed().z)
-						/ (mass + m.getMass());
-				Vector3d tmpSpeed = new Vector3d(0,0,0); 
-				tmpSpeed.add(new Vector3d(v1x, v1y, v1z));
-				tmpSpeed.sub(speed);
-				accel.add(tmpSpeed);
-			}
+			double Cr = parameters.getTypeOfImpact();
+			double v1x = (Cr * m.getMass() * (m.getSpeed().x - speed.x) + mass
+					* speed.x + m.getMass() * m.getSpeed().x)
+					/ (mass + m.getMass());
+			double v1y = (Cr * m.getMass() * (m.getSpeed().y - speed.y) + mass
+					* speed.y + m.getMass() * m.getSpeed().y)
+					/ (mass + m.getMass());
+			double v1z = (Cr * m.getMass() * (m.getSpeed().z - speed.z) + mass
+					* speed.z + m.getMass() * m.getSpeed().z)
+					/ (mass + m.getMass());
+			Vector3d tmpSpeed = new Vector3d(0, 0, 0);
+			tmpSpeed.add(new Vector3d(v1x, v1y, v1z));
+			tmpSpeed.sub(speed);
+			accel.add(tmpSpeed);
 		}
 	}
 
